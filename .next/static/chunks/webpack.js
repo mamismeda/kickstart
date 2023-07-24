8bdd38aae26022e65292dc8a5be41e274a3c9579
mamismeda <amamismeda@gmail.com>
2023-07-24 19:49:14 +0400

    css fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "89cb19a133d18a15"; }
+/******/ 		__webpack_require__.h = function() { return "d59693f8de955968"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
