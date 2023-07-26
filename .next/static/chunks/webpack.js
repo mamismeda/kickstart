e431d2d3a98c0b4684d9ba7128c319009687b8ea
mamismeda <amamismeda@gmail.com>
2023-07-26 23:26:55 +0400

    testing submittal

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "db15097686220316"; }
+/******/ 		__webpack_require__.h = function() { return "b6b227c26ae8e447"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
