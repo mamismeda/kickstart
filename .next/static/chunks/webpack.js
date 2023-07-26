44dea5a81f6583d1be6e1014230b8cce919ee1f6
mamismeda <amamismeda@gmail.com>
2023-07-26 23:45:31 +0400

    button spinner

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e91a51159efcf4d"; }
+/******/ 		__webpack_require__.h = function() { return "73e10db959e8ecb6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
