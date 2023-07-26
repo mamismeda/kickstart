4453ccb6869a7b9af53aac53dc1a1f6711564ce6
mamismeda <amamismeda@gmail.com>
2023-07-26 23:09:08 +0400

    form creation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d59693f8de955968"; }
+/******/ 		__webpack_require__.h = function() { return "db15097686220316"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.fdca22ffa61be917.hot-update.js
+++ .next/static/webpack/webpack.fdca22ffa61be917.hot-update.js
@@ -23,7 +23,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "8124b6c1eb27be2d"; }
+/******/ 	__webpack_require__.h = function() { return "bcf4fb9906dce51c"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
