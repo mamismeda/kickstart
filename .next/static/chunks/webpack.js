58b908c7c870f030e05edd39952fb111b388903f
mamismeda <amamismeda@gmail.com>
2023-07-27 15:52:29 +0400

    next routes setup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "73e10db959e8ecb6"; }
+/******/ 		__webpack_require__.h = function() { return "18909ba842b67d25"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
