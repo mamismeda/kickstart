6cc430df81a00c9f35db0ee4b53d7ca21266ac1a
mamismeda <amamismeda@gmail.com>
2023-07-27 16:01:02 +0400

    preparing server

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "18909ba842b67d25"; }
+/******/ 		__webpack_require__.h = function() { return "0673c42099b14fba"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ server.js
@@ -0,0 +1,17 @@
+const { createServer } = require("http");
+const next = require("next");
+
+const app = next({
+  dev: process.env.NODE_ENV !== "production",
+});
+
+
+const routes = require('./routes');
+const hanlder = routes.getRequestHandler(app);
+
+app.prepare().then(() => {
+    createServer(hanlder).listen(3000, (err) => {
+        if(err) throw err;
+        console.log('Ready on localhost:3000');
+    })
+})
