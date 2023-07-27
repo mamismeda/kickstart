ff23e66fa92df10ac395ac548342dc72d3bc47e8
mamismeda <amamismeda@gmail.com>
2023-07-27 16:05:44 +0400

    automatic navigation

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0673c42099b14fba"; }
+/******/ 		__webpack_require__.h = function() { return "dfee05fe1963c496"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
