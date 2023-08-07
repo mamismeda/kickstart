0715b6255c00c8790412add352fba634b17d796c
mamismeda <amamismeda@gmail.com>
2023-08-07 15:42:13 +0400

    added more route

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4809b296cb1ebb23"; }
+/******/ 		__webpack_require__.h = function() { return "6b205f9b3612babe"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
