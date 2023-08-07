d79cbec339f2e3817002b82e657159be5304705f
mamismeda <amamismeda@gmail.com>
2023-08-07 17:40:34 +0400

    communication

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a219bf8ef8ef2e30"; }
+/******/ 		__webpack_require__.h = function() { return "6b21403c21ff324b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
