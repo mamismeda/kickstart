265481cef1dd7603c8e5c64abac0962225fe8db1
mamismeda <amamismeda@gmail.com>
2023-08-07 17:33:22 +0400

    grid layouts

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9e9577e31fe73154"; }
+/******/ 		__webpack_require__.h = function() { return "ca630df501a70032"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
