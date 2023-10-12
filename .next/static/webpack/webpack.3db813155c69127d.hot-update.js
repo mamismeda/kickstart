92e2e0cad50d549e3a699bf3b4677b329b8059f7
mamismeda <amamismeda@gmail.com>
2023-10-12 19:55:21 +0400

    continue web3

--- .next/static/webpack/webpack.3db813155c69127d.hot-update.js
+++ .next/static/webpack/webpack.3db813155c69127d.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c69fe35dd3b89117"; }
+/******/ 	__webpack_require__.h = function() { return "bf46a4cf83784402"; }
 /******/ }();
 /******/ 
 /******/ }
--- .next/static/webpack/webpack.ded0226ead92dfc5.hot-update.js
+++ .next/static/webpack/webpack.ded0226ead92dfc5.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "c22e3edecc14a6fe"; }
+/******/ 	__webpack_require__.h = function() { return "3db813155c69127d"; }
 /******/ }();
 /******/ 
 /******/ }
