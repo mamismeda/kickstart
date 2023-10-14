28e7566f5c90e406d6eea156ae6fc005eeb3b97f
mamismeda <amamismeda@gmail.com>
2023-10-14 21:42:31 +0400

    Renderering a table

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "d7e4f51fa497e4e8"; }
+/******/ 		__webpack_require__.h = function() { return "01e343ad5390a322"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Button } from "semantic-ui-react";
+import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
 import Layout from "../../../components/layout";
 import Campaign from "../../../ethereum/campaign";
@@ -11,7 +11,7 @@ class RequestIndex extends Component {
     const requestCount = await campaign.methods.getRequestsCount().call();
 
     const requests = await Promise.all(
-      Array(requestCount)
+      Array(parseInt(requestCount))
         .fill()
         .map((element, index) => {
           return campaign.methods.requests(index).call();
@@ -21,6 +21,7 @@ class RequestIndex extends Component {
     return { address, requests, requestCount };
   }
   render() {
+    const { Header, Row, HeaderCell, Body } = Table;
     return (
       <Layout>
         <h3>requests</h3>
@@ -29,6 +30,19 @@ class RequestIndex extends Component {
             <Button primary> Add Request</Button>
           </a>
         </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approvers</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+        </Table>
       </Layout>
     );
   }
